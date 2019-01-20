c7d86ab35c0b68c80b5aec433fb76b670a42013e
Ankita-Upadhyay <ankitaupadhyay66@gmail.com>
2019-01-20 02:54:21 -0800

    Updates

---
+++ precache-manifest.71d17cd5d2fd548520509b7abf6dc0a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "56ed497d1ce646411658",
+    "url": "/Ankita-Upadhyay/cruzhacks2019/static/js/runtime~main.56ed497d.js"
+  },
+  {
+    "revision": "3f2e9f0c95f19acec592",
+    "url": "/Ankita-Upadhyay/cruzhacks2019/static/js/main.3f2e9f0c.chunk.js"
+  },
+  {
+    "revision": "f39f92cfbb80b67ca402",
+    "url": "/Ankita-Upadhyay/cruzhacks2019/static/js/1.f39f92cf.chunk.js"
+  },
+  {
+    "revision": "3f2e9f0c95f19acec592",
+    "url": "/Ankita-Upadhyay/cruzhacks2019/static/css/main.9a31d021.chunk.css"
+  },
+  {
+    "revision": "f39f92cfbb80b67ca402",
+    "url": "/Ankita-Upadhyay/cruzhacks2019/static/css/1.d9ccd230.chunk.css"
+  },
+  {
+    "revision": "2626905fab30a71382ba1d757b2dbffd",
+    "url": "/Ankita-Upadhyay/cruzhacks2019/index.html"
+  }
+];
